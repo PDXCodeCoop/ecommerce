c8e1e990f2162f95b22029315d91e1d70f9e4f59
Braden Jageman <neurobomber@gmail.com>
2016-03-30 00:19:30 -0700

    added price to options in template

--- views_utils.py
+++ views_utils.py
@@ -12,17 +12,7 @@ def getPostList(request, value):
     if value in request.POST:
         return request.POST.getlist(value)
     else:
-        return None
-
-def dict_compare(d1, d2):
-    d1_keys = set(d1.keys())
-    d2_keys = set(d2.keys())
-    intersect_keys = d1_keys.intersection(d2_keys)
-    added = d1_keys - d2_keys
-    removed = d2_keys - d1_keys
-    modified = {o : (d1[o], d2[o]) for o in intersect_keys if d1[o] != d2[o]}
-    same = set(o for o in intersect_keys if d1[o] == d2[o])
-    return added, removed, modified, same
+        return []
 
 def findDuplicateDictInList(dict_compare, dict_list, ignore_keys = []):
     dict_compare_tmp = copy.deepcopy(dict_compare)
