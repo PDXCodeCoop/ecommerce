39c639be5d7bc69b1342cbfe856f6149a95ab8ba
Braden Jageman <neurobomber@gmail.com>
2016-04-07 15:19:25 -0700

    rest api bs

--- serializers.py
+++ serializers.py
@@ -1,12 +1,18 @@
 from django.forms import widgets
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from models import Product, Option, OptionCategory
+from models import Product, Category, Option, OptionCategory
 
 class OptionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Option
         fields = ('title', 'price')
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('title', 'slug')
+
 
 class OptionCategorySerializer(serializers.HyperlinkedModelSerializer):
     option_set = serializers.StringRelatedField(many=True)
@@ -16,6 +22,7 @@ class OptionCategorySerializer(serializers.HyperlinkedModelSerializer):
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     options = OptionCategorySerializer(many=True, read_only=True)
+    category = CategorySerializer()
     class Meta:
         model = Product
         fields = ('__all__')
